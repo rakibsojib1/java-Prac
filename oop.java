73c1484a8a5a4bd8453a4c28bfa0e406fcfc0f79
Rakibul Hasan Sajib <118044314+rakibsojib1@users.noreply.github.com>
2025-12-09 19:53:16 +0600

    practising ...

--- oop.java
+++ oop.java
@@ -11,6 +11,39 @@ class Pen {
     }
 }
 
+class Student{
+    String name;
+    int age;
+
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+    
+    Student(String name, int age){
+        this.name = name;
+        this.age = age;
+
+    }
+    Student(){
+        System.out.println("constroctor called");
+    }
+
+    
+}
+
+class Polym{//Polymorphism
+    public void test(){
+            System.out.println("Im for test");
+    }
+    public void test(String name){
+        System.out.println(name);
+    }
+    public void test(int age){
+        System.out.println("polym age " + age);
+    }
+}
+
 public class Oop {
     public static void main(String args[]){
         System.out.println("Hello me");
@@ -23,5 +56,28 @@ public class Oop {
 
         pen1.printColor();
         pen2.printColor();
+
+
+        Student s1 = new Student();
+        s1.name = "Rakib";
+        s1.age = 25;
+
+        s1.printInfo(); 
+
+        Student s2 = new Student("Hasan",  25);
+
+        s2.printInfo();
+
+
+        Polym polym = new Polym();
+
+        polym.test();
+
+        polym.test("No name");
+        polym.test(25);
+
+
     }
+
+
 }
